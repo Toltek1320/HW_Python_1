62e3c95e1af7b12b94fbb04d0e5f4f9f28b1127d
Ruslan <rus1320@gmail.com>
2023-04-22 17:25:27 +0300

    task_5-1

--- HW_5/task_5-1.py
+++ HW_5/task_5-1.py
@@ -3,11 +3,10 @@
 # Пример:
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8 
-def recApowB(a, b):
+def rec(a, b):
     if b == 0:
         return 1
-    return a * recApowB(a, b - 1)
-
+    return a * rec(a, b - 1)
 a = int(input('Введите число: '))
 b = int(input('Введите степень: '))
-print(recApowB(a, b))
+print(rec(a, b))
